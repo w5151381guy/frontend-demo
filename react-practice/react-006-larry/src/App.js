e9b568adfc55c89495b58307226fe3e5a6258330
Larry Lu <pudding850806@gmail.com>
2017-10-24 12:46:46 +0800

    Use styled-components instead of plain css

--- react-practice/react-006-larry/src/App.js
+++ react-practice/react-006-larry/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import Title from './component/Title'
 import './App.css'
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+`
 
 class TodoList extends Component {
   render() {
-    return 123
+    return (
+      <AppContainer>
+        <Title>Todo List</Title>
+      </AppContainer>
+    )
   }
 }
 
---
+++ react-practice/react-006-larry/src/component/Title.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`color: #222222;`
+
+export default Title
