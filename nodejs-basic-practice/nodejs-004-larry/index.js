898d4fcbcdf920807a8ad4245c98b83af268fe58
Larry Lu <pudding850806@gmail.com>
2017-11-08 14:26:25 +0800

    Change directory name

--- nodejs-basic-practice/nodejs-004-larry/index.js
+++ nodejs-basic-practice/nodejs-004-larry/index.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser')
 const app = express()
 
 /*
-                                                |-> POST /api/item -------|
-                                                |                         |
-Request -> Start Timer -> Parse Body -> Route --+-> GET /api/item/:item --+--> End Timer -> Respond to client
-                                                |                         |
-                                                |-> GET /api/item --------|
+                                        |-> POST /api/item --|
+                                        |                    |
+Request -> Start Timer -> Parse Body ---|       Router       |--> End Timer -> Respond to client
+                                        |                    |
+                                        |-> GET /api/item ---|
 */
 
 // Request come in
@@ -24,11 +24,6 @@ app.use(bodyParser.json())
 /* Route Start */
 app.post('/api/item', (req, res, next) => {
   res.locals.body = `${req.body.id} in body`
-  next()
-})
-
-app.get('/api/item/:item', (req, res, next) => {
-  res.locals.body = `${req.params.item} in params`
   next()
 })
 
