90fe50210083e7876b7d553301853463bf917d62
Larry Lu <pudding850806@gmail.com>
2017-11-08 12:32:27 +0800

    Add comment

--- nodejs-basic-practice/nodejs-005-larry/index.js
+++ nodejs-basic-practice/nodejs-005-larry/index.js
@@ -2,13 +2,26 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+/*
+                                                |-> POST /api/item -------|
+                                                |                         |
+Request -> Start Timer -> Parse Body -> Route --+-> GET /api/item/:item --+--> End Timer -> Respond to client
+                                                |                         |
+                                                |-> GET /api/item --------|
+*/
+
+// Request come in
+
+// Start Timer
 app.use((req, res, next) => {
   res.locals.startTime = new Date()
   next()
 })
 
+// Parse Body
 app.use(bodyParser.json())
 
+/* Route Start */
 app.post('/api/item', (req, res, next) => {
   res.locals.body = `${req.body.id} in body`
   next()
@@ -23,7 +36,10 @@ app.get('/api/item', (req, res, next) => {
   res.locals.body = `${req.query.id} in qs`
   next()
 })
+/* Route End */
 
+// End Timer
+// Respond to client
 app.use((req, res, next) => {
   const { body, startTime } = res.locals
   const processTime = `${new Date() - startTime}ms`
