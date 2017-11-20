3268993be3665c94019246941ca1de811c479ee0
w5151381guy <w5151381guy@gmail.com>
2017-11-20 14:51:16 +0800

    split fs.writeFile

--- nodejs-basic-practice/nodejs-006-andy/file.js
+++ nodejs-basic-practice/nodejs-006-andy/file.js
@@ -11,6 +11,15 @@ function readData() {
     })
 }
 
+function writeData(new_data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('data.txt', new_data, err => {
+            if(err) reject(err)
+            resolve()
+        })
+    })
+}
+
 function findContent(id) {
     return readData().then(data => {
         const data_arr = JSON.parse(`[${data}]`)
@@ -21,44 +30,38 @@ function findContent(id) {
 
 function writeContent(content) {
     const id = uuidv4()
-    return readData().then(data => {
-        const new_data = data + JSON.stringify({id, content})
-        return new Promise((resolve, reject) => {
-            fs.writeFile('data.txt', new_data, err => {
-                if(err) reject(err)
-                resolve(id)
-            })
+    return readData()
+        .then(data => {
+            const new_data = data + JSON.stringify({id, content})
+            return new_data
         })
-    })
+        .then(writeData)
+        .then(() => id)
 }
 
 function updateContent(id, content) {
-    return readData().then(data => {
-        const data_arr = JSON.parse(`[${data}]`)
-        const data_filter = data_arr.filter(el => el.id === id)
-        const new_data = data.toString().replace(data_filter[0].content, content)
-        return new Promise((resolve, reject) => {
-            fs.writeFile('data.txt', new_data, err => {
-                if(err) reject(err)
-                resolve({ok: true})
-            })
+    return readData()
+        .then(data => {
+            const data_arr = JSON.parse(`[${data}]`)
+            const data_filter = data_arr.filter(el => el.id === id)
+            const new_data = data.toString().replace(data_filter[0].content, content)
+            return new_data
         })
-    })
+        .then(writeData)
+        .then(() => JSON.stringify({ok: true}))
 }
 
 function deleteContent(id) {
-    return readData().then(data => {
-        let data_arr = JSON.parse(`[${data}]`)
-        const index = data_arr.findIndex(el => el.id === id)
-        data_arr.splice(index, 1)
-        let data_string = JSON.stringify(data_arr)
-        return new Promise((resolve, reject) => {
-            fs.writeFile('data.txt', data_string.slice(1, data_string.length-1), err => {
-                if(err) reject(err)
-                resolve({ok: true})
-            })
+    return readData()
+        .then(data => {
+            let data_arr = JSON.parse(`[${data}]`)
+            const index = data_arr.findIndex(el => el.id === id)
+            data_arr.splice(index, 1)
+            let data_string = JSON.stringify(data_arr)
+            return data_string.slice(1, data_string.length-1)
         })
-    }) 
+        .then(writeData)
+        .then(() => JSON.stringify({ok: true}))
 }
 
 module.exports = {findContent, writeContent, updateContent, deleteContent}
