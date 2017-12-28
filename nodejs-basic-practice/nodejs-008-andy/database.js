226846014e5b7d4e3c6e68dcbe1772cf8c76a63f
Andy Chen <w5151381guy@gmail.com>
2017-12-28 11:20:28 +0800

    test database.js

--- nodejs-basic-practice/nodejs-008-andy/database.js
+++ nodejs-basic-practice/nodejs-008-andy/database.js
@@ -7,20 +7,44 @@ MongoClient.connect(config.mongoURI, (err, database) => {
     else db = database
 })
 
-function getData() {
-
+function getData(itemid) {
+    let col = db.collection('practice')
+    return new Promise((resolve, reject) => {
+        col.find({'_id': itemid}).toArray((err, result) => {
+            if(err) reject(err)
+            else resolve(result)
+        })
+    })
 }
 
-function insertData() {
-
+function insertData(content) {
+    let col = db.collection('practice')
+    return new Promise((resolve, reject) => {
+        col.insertOne(JSON.stringify({content}), (err, result) => {
+            if(err) reject(err)
+            else resolve(result._id)
+        })
+    })
 }
 
-function updateData() {
-
+function updateData(_id, content) {
+    let col = db.collection('practice')
+    return new Promise((resolve, reject) => {
+        col.updateOne({_id}, {$set: {content}}, (err, result) => {
+            if(err) reject(err)
+            else resolve({ok: 'true'})
+        })
+    })
 }
 
-function deleteData() {
-
+function deleteData(_id) {
+    let col = db.collection('practice')
+    return new Promise((resolve, reject) => {
+        col.remove({_id}, (err, result) => {
+            if(err) reject(err)
+            else resolve({ok: 'true'})
+        })
+    })
 }
 
 module.exports = {getData, insertData, updateData, deleteData}
