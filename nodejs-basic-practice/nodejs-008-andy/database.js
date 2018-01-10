3bbfc4e7ef2498316d0116bc0c9d38bd6d7a5373
Larry Lu <pudding850806@gmail.com>
2018-01-10 14:11:24 +0800

    Fix typo

--- nodejs-basic-practice/nodejs-008-andy/database.js
+++ nodejs-basic-practice/nodejs-008-andy/database.js
@@ -1,32 +1,32 @@
-const {MongoClient, ObjectID} = require('mongodb')
+const { MongoClient, ObjectID } = require('mongodb')
 const config = require('./config')
 let col
 
 MongoClient.connect(config.mongoURI, (err, database) => {
-    if(err) console.log(err)
-    else col = database.db('nodejs-practice').collection('practice')
+  if (err) console.log(err)
+  else col = database.db('nodejs-practice').collection('practice')
 })
 
 function getData(_id) {
-    return col
-        .findOne({'_id': ObjectID(_id)})
-        .then(result => result)
-        .catch(err => console.log(err))
+  return col
+    .findOne({ _id: ObjectID(_id) })
+    .then(result => result)
+    .catch(err => console.log(err))
 }
 
 function insertData(content) {
-    return col
-        .insertOne({content})
-        .then(result => result.insertedId)
-        .catch(err => consolr.log(err))
+  return col
+    .insertOne({ content })
+    .then(result => result.insertedId)
+    .catch(err => console.log(err))
 }
 
 function updateData(_id, content) {
-    return col.updateOne({'_id': ObjectID(_id)}, {$set: {content}})
+  return col.updateOne({ _id: ObjectID(_id) }, { $set: { content } })
 }
 
 function deleteData(_id) {
-    return col.findAndRemove({'_id': ObjectID(_id)})
+  return col.findAndRemove({ _id: ObjectID(_id) })
 }
 
-module.exports = {getData, insertData, updateData, deleteData}
+module.exports = { getData, insertData, updateData, deleteData }
